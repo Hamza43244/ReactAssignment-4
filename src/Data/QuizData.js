7c27d87f6ecaca80aa935012f4b5510115c074a9
Hamza43244 <hamza.43714@iqra.edu.pk>
2023-02-13 20:12:59 +0500

    first commit

---
+++ src/Data/QuizData.js
@@ -0,0 +1,28 @@
+export const QuizData = [
+    
+    {
+        question: "What does XML stand for?",
+        options:[ "eXtensible Markup Language","eXecutable Multiple Language","eXTra Multi-Program Language","eXamine Multiple Language"],
+        answer: 1
+    },    
+    {
+        question: "Which language runs in a web browser?",
+        options:[ "Java", "C", "Python", "JavaScript"],
+        answer: 4
+    },
+    {
+        question: "What does CSS stand for?",
+        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "none of the above"],
+        answer:2
+    },
+];
